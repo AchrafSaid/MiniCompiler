4c75526b38f47681158f81a3b5db98350676819a
Achraf said <ashraf.said@msa.edu.eg>
2026-04-26 22:23:12 +0300

    First Real Commit

--- MiniCompiler/Form1.Designer.cs
+++ MiniCompiler/Form1.Designer.cs
@@ -28,19 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
+            textBox1 = new TextBox();
+            button1 = new Button();
+            listBox1 = new ListBox();
             SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(32, 60);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ScrollBars = ScrollBars.Vertical;
+            textBox1.Size = new Size(319, 254);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveBorder;
+            button1.Location = new Point(268, 359);
+            button1.Name = "button1";
+            button1.Size = new Size(212, 41);
+            button1.TabIndex = 1;
+            button1.Text = "Compile";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(482, 60);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(253, 259);
+            listBox1.TabIndex = 2;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(listBox1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private ListBox listBox1;
     }
 }
--- MiniCompiler/Form1.cs
+++ MiniCompiler/Form1.cs
@@ -4,21 +4,33 @@ namespace MiniCompiler
 {
     public partial class Form1 : Form
     {
+        string[] identifiers = { "int", "float", "string","double","char","bool" };
+        string[] operators ={"+", "-", "/", "%", "*","&&", "||", "<", ">", "=", "!"};
+        string[] brackets ={"(", ")", "{", "}"};
+        string[] punctuation ={",", ";"};
+        string[] reservedWords ={"for", "while", "if", "do","return", "break", "continue", "end"};
+
+
         public Form1()
         {
             InitializeComponent();
 
-            int i;
-            int x= 0;
-            
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            if(true)
-            {
-                MessageBox.Show("Hello World");
-            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string code = textBox1.Text;
+
         }
     }
 }
