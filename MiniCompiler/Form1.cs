548d3cd9890d9df518b6cbb5d8ddb03bbaae44fd
AshrafElnaggar <ashraf.said@msa.edu.eg>
2026-04-29 06:57:47 +0300

    Phase 1 Final Update

--- MiniCompiler/Form1.cs
+++ MiniCompiler/Form1.cs
@@ -11,8 +11,7 @@ namespace MiniCompiler
         string[] punctuation = { ",", ";" };
         string[] reservedWords = { "for", "while", "if", "do", "return", "break", "continue", "end" };
 
-        int loc = 0;
-        string abudubi="";
+        string abudubi = "";
         public Form1()
         {
             InitializeComponent();
@@ -79,7 +78,7 @@ namespace MiniCompiler
             button1.Height = 35;
 
             button1.Left = this.ClientSize.Width - button1.Width - margin;
-            button1.Top = this.ClientSize.Height - button1.Height - 20-20;
+            button1.Top = this.ClientSize.Height - button1.Height - 20 - 20;
 
             // Hover
             button1.MouseEnter += (s, e) =>
@@ -97,21 +96,20 @@ namespace MiniCompiler
         {
 
         }
-        string goodjob="";
+        string goodjob = "";
         private void button1_Click(object sender, EventArgs e)
         {
 
-            string code = textBox1.Text+" ";
+            string code = textBox1.Text + " ";
             bool sympol = false;
-           // MessageBox.Show(code);
-            for (int i = 0; i <code.Length; i++)
+            for (int i = 0; i < code.Length; i++)
             {
                 sympol = false;
                 if (code[i] != ' ' && code[i] != '\n' && code[i] != '\r')
                 {
-                    for(int j = 0; j < operators.Length; j++)
+                    for (int j = 0; j < operators.Length; j++)
                     {
-                        if (""+code[i] == operators[j])
+                        if ("" + code[i] == operators[j])
                         {
                             sympol = true;
                             break;
@@ -119,7 +117,7 @@ namespace MiniCompiler
                     }
                     for (int j = 0; j < brackets.Length; j++)
                     {
-                        if (""+code[i] == brackets[j])
+                        if ("" + code[i] == brackets[j])
                         {
                             sympol = true;
                             break;
@@ -127,7 +125,7 @@ namespace MiniCompiler
                     }
                     for (int j = 0; j < punctuation.Length; j++)
                     {
-                        if (""+code[i] == punctuation[j])
+                        if ("" + code[i] == punctuation[j])
                         {
                             sympol = true;
                             break;
@@ -139,7 +137,7 @@ namespace MiniCompiler
                         for (int n = 0; n < identifiers.Length; n++)
                         {
                             if (abudubi == identifiers[n])
-                            { 
+                            {
                                 goodjob = "identifier";
                                 break;
                             }
@@ -147,7 +145,7 @@ namespace MiniCompiler
                         }
                         for (int n = 0; n < reservedWords.Length; n++)
                         {
-                            if (abudubi == reservedWords[n]) 
+                            if (abudubi == reservedWords[n])
                             {
                                 goodjob = "reservedWord";
                                 break;
@@ -166,14 +164,18 @@ namespace MiniCompiler
                         if (goodjob == "" && abudubi != "")
                         {
                             if (isNumber)
+                            {
                                 goodjob = "Number";
+                            }
                             else
+                            {
                                 goodjob = "Variable";
+                            }
                         }
 
-                        if (abudubi!= "")
+                        if (abudubi != "")
                         {
-                             listBox1.Items.Add(abudubi + " is " + goodjob);
+                            listBox1.Items.Add(abudubi + " is " + goodjob);
                         }
                         abudubi = "";
 
@@ -181,30 +183,30 @@ namespace MiniCompiler
                         goodjob = "";
                         for (int n = 0; n < operators.Length; n++)
                         {
-                            if (abudubi == operators[n]) 
-                            { 
+                            if (abudubi == operators[n])
+                            {
                                 goodjob = "operator";
-                                break; 
+                                break;
                             }
                         }
                         for (int n = 0; n < brackets.Length; n++)
                         {
-                            if (abudubi == brackets[n]) 
-                            { 
-                                goodjob = "bracket"; 
-                                break; 
+                            if (abudubi == brackets[n])
+                            {
+                                goodjob = "bracket";
+                                break;
                             }
                         }
                         for (int n = 0; n < punctuation.Length; n++)
                         {
                             if (abudubi == punctuation[n])
-                            { 
-                                goodjob = "punctuation"; 
+                            {
+                                goodjob = "punctuation";
                                 break;
                             }
                         }
                         listBox1.Items.Add(abudubi + " is " + goodjob);
-                        abudubi = "";  
+                        abudubi = "";
                         sympol = false;
                     }
                     else
@@ -216,12 +218,12 @@ namespace MiniCompiler
                 {
                     //comparing
                     goodjob = "";
-                    for (int n=0;n< identifiers.Length;n++)
+                    for (int n = 0; n < identifiers.Length; n++)
                     {
                         if (abudubi == identifiers[n])
                         {
-                             goodjob = "identifier";
-                             break;
+                            goodjob = "identifier";
+                            break;
                         }
                     }
 
@@ -281,12 +283,12 @@ namespace MiniCompiler
                             goodjob = "Variable";
                         }
                     }
-                    if (goodjob !=  "")
+                    if (goodjob != "")
                     {
-                    listBox1.Items.Add(abudubi+" is "+goodjob);
+                        listBox1.Items.Add(abudubi + " is " + goodjob);
                     }
                     abudubi = "";
-                    
+
                 }
             }
         }
