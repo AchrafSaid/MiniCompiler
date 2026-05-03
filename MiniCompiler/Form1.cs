eedd02eac765d2cfe69599eb32e7abfdd4debb71
Achraf said <ashraf.said@msa.edu.eg>
2026-05-03 20:48:36 +0300

    Phase 2 initialise

--- MiniCompiler/Form1.cs
+++ MiniCompiler/Form1.cs
@@ -91,16 +91,9 @@ namespace MiniCompiler
         {
 
         }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        void Scanner(string code)
         {
-
-        }
-        string goodjob = "";
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-            string code = textBox1.Text + " ";
+            string goodjob = "";
             bool sympol = false;
             for (int i = 0; i < code.Length; i++)
             {
@@ -291,6 +284,15 @@ namespace MiniCompiler
 
                 }
             }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string code = textBox1.Text + " ";
+            Scanner(code);
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
